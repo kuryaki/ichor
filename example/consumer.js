bdf8b21ca794a8475a48cfa8e2b69bda93608cad
David Roncancio <david@roncancio.me>
2019-05-14 01:22:33 -0400

    rename

--- example/consumer.js
+++ example/consumer.js
@@ -1,14 +1,14 @@
 
-const Ichor = require('../lib');
+const QStream = require('../lib');
 
-const ichor = Ichor(/** Redis Connection Options see https://github.com/NodeRedis/node_redis#rediscreateclient */);
+const qstream = QStream(/** Redis Connection Options see https://github.com/NodeRedis/node_redis#rediscreateclient */);
 
 const topicName = 'mytopic'; // TOPIC
 const groupName = 'mygroup'; // CONSUMER GROUP
 
 (async () => {
 
-    const group = await ichor.group(topicName, groupName);
+    const group = await qstream.group(topicName, groupName);
     group.consume(async (data) => {
         console.log({ data });
         return true;
