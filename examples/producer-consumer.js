f31aa9c237618b8528c18e0d5bba95f1dbaa9150
David Roncancio <david@roncancio.me>
2019-05-14 17:25:34 -0400

    code reorganize + better connection handling

--- examples/producer-consumer.js
+++ examples/producer-consumer.js
@@ -23,4 +23,19 @@ setInterval(async () => {
     console.log({ counter, id });
 
     counter++;
-}, 5000);
+}, 1000);
+
+
+//consumer
+const topicName = 'mytopic'; // TOPIC
+const groupName = 'mygroup'; // CONSUMER GROUP
+
+(async () => {
+
+    const group = await qstream.group(topicName, groupName);
+    group.consume(async (data) => {
+        console.log(data);
+        return true;
+    });
+})();
+
