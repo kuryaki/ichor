21273a1a1180a7f318d842163cb537c1631c76f5
David Roncancio <david@roncancio.me>
2019-05-18 09:53:19 -0400

    feat: default start at of group to latest message instead of start of stream

--- examples/consumer.js
+++ examples/consumer.js
@@ -4,7 +4,7 @@ const QStream = require('../lib');
 const qstream = QStream(/** Redis Connection Options see https://github.com/NodeRedis/node_redis#rediscreateclient */);
 
 const topicName = 'mytopic'; // TOPIC
-const groupName = 'mygroup'; // CONSUMER GROUP
+const groupName = 'agroup3'; // CONSUMER GROUP
 
 (async () => {
 
